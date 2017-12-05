1ad8e6a345b15db933af7cc618b8ec42e9efbd02
Harland Duman <dumanjacob@gmail.com>
2017-12-05 03:32:42 -0800

    Added webpack dev server

--- flask/server.py
+++ flask/server.py
@@ -1,11 +1,13 @@
 import sys
 from flask import Flask, render_template
-sys.path.append("..")
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static/dist", template_folder="static")
+# sys.path.append("..")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
