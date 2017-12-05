3f61c8ac7d5875299a701b6d45270230416640ca
Harland Duman <dumanjacob@gmail.com>
2017-12-05 02:39:03 -0800

    React is up and running

--- flask/server.py
+++ flask/server.py
@@ -1,11 +1,11 @@
 import sys
-from flask import Flask
+from flask import Flask, render_template
 sys.path.append("..")
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
--- flask/static/js/index.jsx
+++ flask/static/js/index.jsx
@@ -1,1 +1,5 @@
-alert("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App/>, document.getElementById("content"));
