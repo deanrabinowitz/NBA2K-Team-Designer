dc18d84eabc0771ca6f21c4f37f662ca91a2a5a8
Dean Rabinowitz <drabinow@ucsc.edu>
2017-12-04 21:39:17 -0800

    0 <= x <= inf

--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ def player_solver(players, attribute_bounds):
     def create_constraint(attribute, bounds):
         min_value, max_value = bounds
         def constraint(player):
-            return max_value >= player[attribute] >= min_value
+            return min_value <= player[attribute] <= max_value
         return constraint
 
     problem = Problem()
@@ -33,7 +33,7 @@ def team_solver(pg, sg, pf, sf, c, token_cap):
 #     "mid_range": 10,
 #     "close_range": 10
 # }, {
-#     "name": "max",
+#     "name": "pekk",
 #     "3pt": 4,
 #     "mid_range": 4,
 #     "close_range": 5
