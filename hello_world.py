5c57412816ec8ee9063a020d5675571884ec1178
Dean Rabinowitz <drabinow@ucsc.edu>
2017-12-07 23:47:09 -0800

    random commit

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,16 @@ import pandas as p
 import constraint as const
 from Player import Player
 
-stats = p.read_csv("playerstats.csv")
-statsadvanced = p.read_csv("playerstatsadvanced.csv")
 
-merged_stats = p.merge(left=stats, right=statsadvanced, how='outer', on='Rk')
+############################### Data Scraping #################################
+
+stats = p.read_csv("playerstats.csv", index_col=False)
+statsadvanced = p.read_csv("playerstatsadvanced.csv", index_col=False)
+merged_stats = p.merge(left=stats, right=statsadvanced, how='outer',
+                       on=['Rk', 'Player', 'Pos', 'Age', 'Tm', 'G', 'MP'])
+
+
+############################## Data Processing ################################
 
 # Define some lists.
 sg_list = []
@@ -31,29 +37,23 @@ problem = const.Problem()
 # Need to create a list of player objects for each position, populate variables that way
 # The commented code below will now work.
 
-print(sg)
 
-# it = sg.itertuples()
-# row = next(it)
-# while(row):
-#     try:
-#         player = Player()
-#         player.name = row[NAME]
-#         player.position = row[POS]
-#         player.age = row[3]
-#         player.team = row[4]
-#         player.games_played = row[5]
-#
-#         sg_list.append(player)
-#
-#         row = next(it)
-#         # print("name: {}\nposition: {}\nage: {}\nteam: {}\ngp: {}".format(
-#         #     player.name, player.position, player.age, player.team, player.games_played))
-#     except StopIteration:
-#         break
-# problem.addVariable("sg", sg_list)
-# problem.addVariable("pg", [row for row in pg.iterrows()])
-# problem.addVariable("sf", [row for row in sf.iterrows()])
-# problem.addVariable("pf", [row for row in pf.iterrows()])
-# problem.addVariable("c", [row for row in c.iterrows()])
+it = sg.itertuples()
+row = next(it)
+while row:
+    try:
+        player = Player()
+        player.name = row[2].split("\\")[0]
+        player.position = row[3]
+        player.age = row[4]
+        player.team = row[5]
+        player.games_played = row[6]
 
+        sg_list.append(player)
+
+        row = next(it)
+        print("name: {}\nposition: {}\nage: {}\nteam: {}\ngp: {}".format(
+            player.name, player.position, player.age, player.team, player.games_played))
+    except StopIteration:
+        print("stopped it")
+        break
